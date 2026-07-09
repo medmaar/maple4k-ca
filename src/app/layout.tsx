64431bb4a0b26d4379f7633f3743fcfd5a943507
medmaar <mo@maple4k.ca>
2026-07-09 10:11:55 +0000

    Add rotating sales-alert banner component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "./Navbar";
 import Footer from "../components/Footer";
 import FloatingContact from "../components/FloatingContact";
+import SalesBanner from "../components/SalesBanner";
 
 import Animations from "./components/Animations";
 
@@ -104,6 +105,7 @@ export default function RootLayout({
                         <div className="flex-1">{children}</div>
                         <Footer />
                         <FloatingContact />
+                        <SalesBanner />
                         <Animations />
                 </body>
           </html>
